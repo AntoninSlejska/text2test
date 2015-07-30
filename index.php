e2a9c0e88843d73a7f4dde6e9404e0630958bf21
AntoninSlejska <antonin@slejska.eu>
2015-07-30 16:07:37 +0200

    :lipstick: Adds description

--- index.php
+++ index.php
@@ -17,19 +17,20 @@ echo '<!DOCTYPE html>
 <html>
   <head>
    <title>'.$website_title.'</title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link rel="stylesheet" type="text/css" href="'.$main_css.'">
-    <link rel="stylesheet" type="text/css" href="'.$menu_css.'">
-    <link rel="stylesheet" type="text/css" href="vendor/drmonty/chosen/css/chosen.min.css">
-    <link rel="stylesheet" type="text/css" href="vendor/components/font-awesome/css/font-awesome.min.css">
-    <link rel="shortcut icon" href="templates/'.$template_name.'/favicon.ico" type="image/x-icon">
-    <script type="text/javascript" src="vendor/components/jquery/jquery.min.js"></script>
-    <script type="text/javascript" src="vendor/drmonty/chosen/js/chosen.jquery.min.js"></script>
-    <script type="text/javascript" src="'.$base_url.'i18n/main.js.php"></script>
-    <script type="text/javascript" src="'.$main_js.'"></script>
-    <script type="text/javascript" src="'.$functions_js.'"></script>
-    <!--[if IE]> <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+   <meta name="description" content="Make your text to a test.">
+   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+   <link rel="stylesheet" type="text/css" href="'.$main_css.'">
+   <link rel="stylesheet" type="text/css" href="'.$menu_css.'">
+   <link rel="stylesheet" type="text/css" href="vendor/drmonty/chosen/css/chosen.min.css">
+   <link rel="stylesheet" type="text/css" href="vendor/components/font-awesome/css/font-awesome.min.css">
+   <link rel="shortcut icon" href="templates/'.$template_name.'/favicon.ico" type="image/x-icon">
+   <script type="text/javascript" src="vendor/components/jquery/jquery.min.js"></script>
+   <script type="text/javascript" src="vendor/drmonty/chosen/js/chosen.jquery.min.js"></script>
+   <script type="text/javascript" src="'.$base_url.'i18n/main.js.php"></script>
+   <script type="text/javascript" src="'.$main_js.'"></script>
+   <script type="text/javascript" src="'.$functions_js.'"></script>
+   <!--[if IE]> <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
   </head>
   <body>
     <header>
