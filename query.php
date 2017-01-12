27f955bba69def982bb460b031dc88d2ce8b435e
Antonin Slejska <antonin@slejska.eu>
2017-01-12 10:28:47 +0100

    Update all SQL commands to be compatible with the recent Dibi

--- query.php
+++ query.php
@@ -1,94 +1,115 @@
 <?php
+
 ini_set('display_errors', 1);
 require_once 'vendor/autoload.php';
 require_once 'config.php';
 
-use Dibi\Dibi;
-$db = new DibiConnection($database_configuration);
+dibi::connect($database_configuration);
 
 if (isset($_COOKIE['language'])) {
-  $locale = $db->fetchSingle('SELECT locale FROM Languages WHERE code="'.$_COOKIE['language'].'"');
-  putenv("LC_ALL=$locale");
-  setlocale(LC_ALL, $locale);
-  bindtextdomain($text_domain,$base_dir."/i18n");
-  textdomain($text_domain);
+    $locale = dibi::fetchSingle('SELECT locale FROM [Languages] WHERE [code] = %i', $_COOKIE['language']);
+    putenv("LC_ALL=$locale");
+    setlocale(LC_ALL, $locale);
+    bindtextdomain($text_domain, $base_dir . "/i18n");
+    textdomain($text_domain);
 }
 
 switch (isset($_GET['q']) ? $_GET['q'] : '') {
-  case "selectlanguage":
-    $rows = $db -> query("SELECT code, name FROM Languages WHERE supported = 1");
-    echo "<option value='' selected></option>";
-    foreach ($rows as $row) {
-      echo "<option value='".$row['code']."'>".$row['name']."</option>";
-    }
-    break;
-  case "savetext":
-    $language	= isset($_POST['language']) ? $_POST['language'] : ''; // It will be possible to use Null Coalesce Operator in PHP 7, see: https://wiki.php.net/rfc/isset_ternary
-	  $title		= isset($_POST['title']) ? $_POST['title'] : '';
-		$text		= isset($_POST['text']) ? $_POST['text'] : '';
-		$level		= isset($_POST['level']) ? $_POST['level'] : '';
-		$source		= isset($_POST['source']) ? $_POST['source'] : '';
-		$url		= isset($_POST['url']) ? $_POST['url'] : '';
-		$public		= isset($_POST['pub']) ? $_POST['pub'] : '';
-
-		$db->query("INSERT INTO Texts (language,title,text,level,source,url,public) VALUES ('" . $language . "','" . $title . "','" . $text . "','" . $level . "','" . $source . "','" . $url . "','" . $public . "')");
-        echo $db->getInsertId();;
+    case "selectlanguage":
+        $rows = dibi::query("SELECT code, name FROM [Languages] WHERE [supported] = %i", 1);
+        echo "<option value='' selected></option>";
+        foreach ($rows as $row) {
+            echo "<option value='" . $row['code'] . "'>" . $row['name'] . "</option>";
+        }
         break;
-  case "savetest":
-		$secret		= uniqid();
-    $language	= isset($_POST['language']) ? $_POST['language'] : '';
-		$text		= isset($_POST['text']) ? $_POST['text'] : '';
-		$testtype	= isset($_POST['testtype']) ? $_POST['testtype'] : '';
-		$test		= isset($_POST['test']) ? $_POST['test'] : '';
-		$solution	= isset($_POST['solution']) ? $_POST['solution'] : '';
+    case "savetext":
+        $text_arr = [
+            'language' => isset($_POST['language']) ? $_POST['language'] : '', // It will be possible to use Null Coalesce Operator in PHP 7, see: https://wiki.php.net/rfc/isset_ternary
+            'title' => isset($_POST['title']) ? $_POST['title'] : '',
+            'text' => isset($_POST['text']) ? $_POST['text'] : '',
+            'level' => isset($_POST['level']) ? $_POST['level'] : '',
+            'source' => isset($_POST['source']) ? $_POST['source'] : '',
+            'url' => isset($_POST['url']) ? $_POST['url'] : '',
+            'public' => isset($_POST['pub']) ? $_POST['pub'] : '',
+        ];
 
-		$db->query("INSERT INTO Tests (secret,language,text,testtype,test,solution) VALUES ('" . $secret . "','" . $language . "','" . $text . "','" . $testtype . "','" . $test . "','" . $solution . "')");
-    echo $secret;
-    break;
-  case "sendtest":
-      $secret     = isset($_POST['secretId']) ? $_POST['secretId'] : '';
-      $level      = isset($_POST['level']) ? $_POST['level'] : '';
-      $public     = isset($_POST['pub']) ? $_POST['pub'] : '';
+        try {
+            dibi::query("INSERT INTO [Texts]", $text_arr);
+            echo $db->getInsertId();
+        } catch (DibiException $ex) {
+            echo get_class($e), ': ', $e->getMessage(), "\n";
+        }
 
-      $db->query("UPDATE Tests SET level='".$level."', save='1', public='".$public."' WHERE secret='".$secret."'");
-      echo _("You can find the test on the URL").": <a href='".$base_url . $secret."/'>".$base_url . $secret."/</a>";
-      break;
-  case "selecttext":
-  	$language = isset($_GET['la']) ? $_GET['la'] : $default_language;
-  	$level = isset($_GET['le']) ? $_GET['le'] : $default_level;
+        break;
+    case "savetest":
+        $test_arr = [
+            'secret' => uniqid(),
+            'language' => isset($_POST['language']) ? $_POST['language'] : '', // It will be possible to use Null Coalesce Operator in PHP 7, see: https://wiki.php.net/rfc/isset_ternary
+            'text' => isset($_POST['text']) ? $_POST['text'] : '',
+            'testtype' => isset($_POST['testtype']) ? $_POST['testtype'] : '',
+            'test' => isset($_POST['test']) ? $_POST['test'] : '',
+            'solution' => isset($_POST['solution']) ? $_POST['solution'] : '',
+        ];
 
-  	$res = $db->query('SELECT * FROM Texts WHERE language="'.$language.'" AND level="'.$level.'"');
-		$pairs = $res->fetchPairs('id', 'title');
-		echo "<option value='' class='optionTexts' selected></option>";
-		foreach ($pairs as $id => $title) {
-			echo "<option value='$id' class='optionTexts'>$title</option>";
-		}
-    break;
-  case "selecttesttype":
-  	$language = isset($_GET['la']) ? $_GET['la'] : $default_language;
+        try {
+            dibi::query("INSERT INTO [Tests]", $test_arr);
+            echo $test_arr['secret'];
+        } catch (DibiException $ex) {
+            echo get_class($e), ': ', $e->getMessage(), "\n";
+        }
 
-  	$res = $db->query('SELECT * FROM TestTypes WHERE language="'.$language.'" AND public=1');
-		$pairs = $res->fetchPairs('id', 'title');
-		echo "<option value='' class='optionTestTypes' selected></option>";
-		foreach ($pairs as $id => $title) {
-			echo "<option value='$id' class='optionTestTypes'>$title</option>";
-		}
+        break;
+    case "sendtest":
+        $test_arr = [
+            'secret' => isset($_POST['secretId']) ? $_POST['secretId'] : '',
+            'level' => isset($_POST['level']) ? $_POST['level'] : '',
+            'public' => isset($_POST['pub']) ? $_POST['pub'] : '',
+        ];
+        dibi::query("UPDATE `Tests` SET ", $test_arr, " WHERE `secret` = %i", $test_arr['secret']);
+        echo _("You can find the test on the URL") . ": <a href='" . $base_url . $test_arr['secret'] . "/'>" . $base_url . $test_arr['secret'] . "/</a>";
+        break;
+    case "selecttext":
+        $language = isset($_GET['la']) ? $_GET['la'] : $default_language;
+        $level = isset($_GET['le']) ? $_GET['le'] : $default_level;
+
+        $res = dibi::query('SELECT * FROM [Texts] WHERE %and', [
+                    'language' => $language,
+                    'level' => $level,
+        ]);
+        echo dibi::$sql;
+        $pairs = $res->fetchPairs('id', 'title');
+        echo "<option value='' class='optionTexts' selected></option>";
+        foreach ($pairs as $id => $title) {
+            echo "<option value='$id' class='optionTexts'>$title</option>";
+        }
+        break;
+    case "selecttesttype":
+        $language = isset($_GET['la']) ? $_GET['la'] : $default_language;
+
+        $res = dibi::query('SELECT * FROM [TestTypes] WHERE %and', [
+                    'language' => $language,
+                    'public' => 1,
+        ]);
+        $pairs = $res->fetchPairs('id', 'title');
+        echo "<option value='' class='optionTestTypes' selected></option>";
+        foreach ($pairs as $id => $title) {
+            echo "<option value='$id' class='optionTestTypes'>$title</option>";
+        }
         break;
     case "inserttext":
-    	$id = isset($_GET['id']) ? $_GET['id'] : '0';
-    	$text = $db->fetchSingle('SELECT text FROM Texts WHERE id="'.$id.'"');
-		echo $text;
+        $id = isset($_GET['id']) ? $_GET['id'] : '0';
+        $text = dibi::fetchSingle('SELECT text FROM [Texts] WHERE id = %i', $id);
+        echo $text;
         break;
     case "gettesttype":
-    	$id = isset($_GET['id']) ? $_GET['id'] : '0';
-    	$row = $db->fetch('SELECT * FROM TestTypes WHERE id="'.$id.'" LIMIT 1');
-    	$terms		= $row->terms;
-    	$function	= $row->function;
-    	$json		= array('terms' => $terms, 'functionName' => $function);
-    	header("Content-Type: application/json", true);
-    	echo json_encode($json);
+        $id = isset($_GET['id']) ? $_GET['id'] : '0';
+        $row = dibi::fetch('SELECT * FROM [TestTypes] WHERE [id] = %i', $id);
+        $terms = $row->terms;
+        $function = $row->function;
+        $json = array('terms' => $terms, 'functionName' => $function);
+        header("Content-Type: application/json", true);
+        echo json_encode($json);
     case "":
         break;
 }
-
 ?>
--- test.php
+++ test.php
@@ -2,110 +2,110 @@
 require_once 'vendor/autoload.php';
 require_once 'config.php';
 
-use Dibi\Dibi;
-$db = new DibiConnection($database_configuration);
+dibi::connect($database_configuration);
 
 $secret = isset($_GET['secret']) ? $_GET['secret'] : '';
 
 if ($minimise_code) {
-	$minimise = ".min";
+    $minimise = ".min";
 } else {
-	$minimise = "";
+    $minimise = "";
 }
 
-$row = $db->fetch('SELECT * FROM Tests WHERE secret="'.$secret.'" LIMIT 1');
-$language	= $row->language;
-$textId 	= (int) $row->text;
-$testTypeId	= (int) $row->testtype;
+$row = dibi::fetch('SELECT * FROM [Tests] WHERE [secret]= %i', $secret);
+
+$language = $row->language;
+$textId = (int) $row->text;
+$testTypeId = (int) $row->testtype;
 $testText = $row->test;
-$solution	= $row->solution;
-$level		= $row->level;
-$save		= (boolean) $row->save;
-$public		= (boolean) $row->public;
+$solution = $row->solution;
+$level = $row->level;
+$save = (boolean) $row->save;
+$public = (boolean) $row->public;
 
 if (strlen($language) == 2) {
-  setcookie('language', $language);
-  $locale = $db->fetchSingle('SELECT locale FROM Languages WHERE code="'.$language.'"');
+    setcookie('language', $language);
+    $locale = dibi::fetchSingle('SELECT locale FROM [Languages] WHERE [code]= %i', $language);
 } else {
-  $language = $default_language;
-  if (isset($_COOKIE['language'])) {
-    $locale = $db->fetchSingle('SELECT locale FROM Languages WHERE code="'.$_COOKIE['language'].'"');
-  } else {
-    setcookie('language', $default_language);
-    $locale = $db->fetchSingle('SELECT locale FROM Languages WHERE code="'.$default_language.'"');
-  }
+    $language = $default_language;
+    if (isset($_COOKIE['language'])) {
+        $locale = dibi::fetchSingle('SELECT locale FROM [Languages] WHERE [code]= %i', $_COOKIE['language']);
+    } else {
+        setcookie('language', $default_language);
+        $locale = dibi::fetchSingle('SELECT locale FROM [Languages] WHERE [code]= %i', $default_language);
+    }
 }
 
 if ($language != "en") {
-  putenv("LC_ALL=$locale");
-  setlocale(LC_ALL, $locale);
-  bindtextdomain($text_domain,$base_dir."/i18n");
-  textdomain($text_domain);
+    putenv("LC_ALL=$locale");
+    setlocale(LC_ALL, $locale);
+    bindtextdomain($text_domain, $base_dir . "/i18n");
+    textdomain($text_domain);
 }
 
 if ($textId > 0) {
-	$row = $db->fetch('SELECT * FROM Texts WHERE id="'.$textId.'" LIMIT 1');
-	$textTitle	= '<div id="title">'.$row->title.'</div>';
-	$textOrigin	= $row->text;
-	$textSource	= '<div id="source">'._("Source").': <a href="'.$row->url.'">'.$row->source.'</a></div>';
+    $row = dibi::fetch('SELECT * FROM [Texts] WHERE [id] = %i', $textId);
+    $textTitle = '<div id="title">' . $row->title . '</div>';
+    $textOrigin = $row->text;
+    $textSource = '<div id="source">' . _("Source") . ': <a href="' . $row->url . '">' . $row->source . '</a></div>';
 } else {
-	$textTitle	= '';
-	$textOrigin	= '';
-	$textSource	= '';
+    $textTitle = '';
+    $textOrigin = '';
+    $textSource = '';
 }
 
-$row = $db->fetch('SELECT * FROM TestTypes WHERE id="'.$testTypeId.'" LIMIT 1');
-$testTitle			= $row->title;
-$testDescription	= $row->description;
-$testTerms			= $row->terms;
-$testScript			= $row->script;
+$row = dibi::fetch('SELECT * FROM [TestTypes] WHERE [id] = %i', $testTypeId);
+$testTitle = $row->title;
+$testDescription = $row->description;
+$testTerms = $row->terms;
+$testScript = $row->script;
 if ($row->css == "") {
-	$ownStyle	= "";
+    $ownStyle = "";
 } else {
-	$ownStyle	= "\n    <link rel='stylesheet' type='text/css' href='".$base_url."scripts/".$row->css.$minimise.".css'>";
+    $ownStyle = "\n    <link rel='stylesheet' type='text/css' href='" . $base_url . "scripts/" . $row->css . $minimise . ".css'>";
 }
 
 $externalCodes = "";
-$rows = $db -> query("SELECT ec.type, ec.src, ec.relative_url FROM ExternalCodes ec INNER JOIN TestTypes2ExternalCodes tt2ec ON ec.id = tt2ec.external_code_id WHERE tt2ec.test_type_id = $testTypeId ORDER BY ec.type, ec.id ASC");
+$rows = dibi::query("SELECT ec.type, ec.src, ec.relative_url FROM ExternalCodes ec INNER JOIN TestTypes2ExternalCodes tt2ec ON ec.id = tt2ec.external_code_id WHERE tt2ec.test_type_id = $testTypeId ORDER BY ec.type, ec.id ASC");
 foreach ($rows as $row) {
-	if ($row['relative_url'] == 1) {
-		$extCodeSrc = $base_url . $row['src'];
-	} else {
-		$extCodeSrc = $row['src'];
-	}
-	switch ($row['type']) {
-		case "js":
-			$externalCodes .= "\n    <script type='text/javascript' src='$extCodeSrc'></script>";
-			break;
-		case "css":
-			$externalCodes .= "\n    <link rel='stylesheet' type='text/css' href='$extCodeSrc'>";
-			break;
-		}
+    if ($row['relative_url'] == 1) {
+        $extCodeSrc = $base_url . $row['src'];
+    } else {
+        $extCodeSrc = $row['src'];
+    }
+    switch ($row['type']) {
+        case "js":
+            $externalCodes .= "\n    <script type='text/javascript' src='$extCodeSrc'></script>";
+            break;
+        case "css":
+            $externalCodes .= "\n    <link rel='stylesheet' type='text/css' href='$extCodeSrc'>";
+            break;
+    }
 }
 
 echo '<!DOCTYPE html>
 <html>
   <head>
-   <title>'.$website_title.'</title>
+   <title>' . $website_title . '</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link rel="shortcut icon" href="'.$base_url.'templates/'.$template_name.'/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="'.$base_url.'templates/'.$template_name.'/css/test'.$minimise.'.css">'.$ownStyle.$externalCodes.'
-    <script type="text/javascript" src="'.$base_url.'i18n/main.js.php"></script>
-    <script type="text/javascript" src="'.$base_url.'scripts/'.$testScript.$minimise.'.js"></script>
+    <link rel="shortcut icon" href="' . $base_url . 'templates/' . $template_name . '/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="' . $base_url . 'templates/' . $template_name . '/css/test' . $minimise . '.css">' . $ownStyle . $externalCodes . '
+    <script type="text/javascript" src="' . $base_url . 'i18n/main.js.php"></script>
+    <script type="text/javascript" src="' . $base_url . 'scripts/' . $testScript . $minimise . '.js"></script>
   </head>
 	<body>
 		<div id="header">
-			<h1>'.$testTitle.'</h1>
-			<div id="description">'.$testDescription.'</div>
+			<h1>' . $testTitle . '</h1>
+			<div id="description">' . $testDescription . '</div>
 			<div id="messages"></div>
 		</div>
 		<div id="mainColumn">
-			'.$textTitle.'
-			<div id="text">'.$testText.'</div>
+			' . $textTitle . '
+			<div id="text">' . $testText . '</div>
 			<div id="origin"></div>
-			'.$textSource.'
+			' . $textSource . '
 		</div>
 		<div id="control"></div>
-		<div id="solution">'.$solution.'</div>'.$tracking_code.'
+		<div id="solution">' . $solution . '</div>' . $tracking_code . '
 	</body>
 </html>';
